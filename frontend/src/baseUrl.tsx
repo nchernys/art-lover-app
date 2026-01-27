1358f104d8f0fc04bddd990f648d93fbeab90725
Natalia Chernysheva <159584009+nchernys@users.noreply.github.com>
2026-01-27 00:53:03 -0500

    feat(base-url) update base url for development and production

--- frontend/src/baseUrl.tsx
+++ frontend/src/baseUrl.tsx
@@ -1,4 +1,4 @@
 export const API_BASE =
-  import.meta.env.REACT_APP_ENV === "production"
-    ? import.meta.env.VITE_API_BASE
-    : "http://localhost:8080";
+  import.meta.env.REACT_APP_ENV === "development"
+    ? "http://localhost:8080"
+    : import.meta.env.VITE_API_BASE;
