9d11c564d3ebb5e27b1eaf2d9a01e41bba767f59
Natalia Chernysheva <159584009+nchernys@users.noreply.github.com>
2026-01-27 03:36:42 -0500

    update github actions yml for frontend deployment on S3 + Cloudfront

--- backend/src/main/java/com/example/art_lover/controller/ArtLoverController.java
+++ backend/src/main/java/com/example/art_lover/controller/ArtLoverController.java
@@ -201,9 +201,9 @@ public class ArtLoverController {
 
 			ResponseCookie cookie = ResponseCookie.from("AUTH_TOKEN", token)
 					.httpOnly(true)
-					.secure(false) // true in production w/ https
+					.secure(true) // true in production w/ https
 					.path("/")
-					.sameSite("Lax") // Strict in production
+					.sameSite("None") // Strict in production
 					.maxAge(60 * 60) // 1 hour
 					.build();
 
